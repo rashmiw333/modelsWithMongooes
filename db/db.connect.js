afd492ca4878a8f7aa195f5a23cf583b359ed895
rashmiw333 <rashmiwankhade99@gmail.com>
2026-06-08 14:50:40 +0530

    create hotel Schrema and add Data to database

---
+++ db/db.connect.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const dns = require("dns");
+dns.setServers(["8.8.8.8","8.8.4.4"]);
+
+const mongoUri = process.env.MONGODB;
+console.log(process.env.MONGODB);
+
+const initializeDatabase = async()=>{
+    await mongoose.connect(mongoUri).then(()=>{
+        console.log("connected to Database");
+    }).catch((error)=>console.log("Error connecting to Database",error));
+};
+
+module.exports = {initializeDatabase};
