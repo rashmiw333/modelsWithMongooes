95c4153960418364ebf50cf625f035976cb55172
rashmiw333 <rashmiwankhade99@gmail.com>
2026-06-08 13:08:44 +0530

    add new movie data in database

--- index.js
+++ index.js
@@ -1,39 +1,32 @@
 const {initializeDatabase} = require("./db/db.connect");
-const fs = require('fs');
- const Car = require("./models/cars.models");
-const { json } = require("stream/consumers");
+ const Movie = require("./models/movie.models");
+
 initializeDatabase();
- const jsonData = fs.readFileSync('cars.json',"utf-8");
-const carsData = JSON.parse(jsonData);
 
-function seedData(){
-    try{
-        for(const carData of carsData){
-            const newCar = new Car({
-                brand: carData.brand,
-                model: carData.model,
-                year: carData.year,
-                bodyStyle: carData.bodyStyle,
-                fuelType: carData.fuelType,
-                transmission: carData.transmission,
-                engine: carData.engine,
-                mileage: carData.mileage,
-                color: carData.color,
-                price: carData.price,
-                condition: carData.condition,
-                description: carData.description,
-                photos:carData.photos,
-                inMarket: carData.inMarket
+        const newMovie = {
+                title: "New Movie",
+                releaseYear: 2023,
+                genre: ["Drama"],
+                director: "Aditya Roy Chopra",
+                actors: ["Actor1","Actor2"],
+                language: "Hindi",
+                country: "India",
+                rating: 6.1,
+                plot: "A young Man and Young Woman fall in love on a trip",
+                awards: "IFA Filmfare Awards",
+                posterUrl: "https://example.com/new-poster1.jpg",
+                trailerUrl: "https://example.com/new-trailer1.mp4",
 
-            });
-           newCar.save();
-           //console.log(newCar);
+        };
+
+        async function createMovie(newMovie){
+            try{
+                const movie = new Movie(newMovie);
+                const saveMovie = await movie.save();
+                console.log(saveMovie,"movie Data")
+            }catch(error){
+                throw error;
+            }
         }
-
-    }catch(error){
-        console.log("Error seeding the Data",error);
-    }
-}
-
-
- seedData();
+           
+ createMovie(newMovie);
